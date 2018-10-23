9fb10809c22b3fd9403c6e5c3dd514927db23f38
Himank Goel <himankgoel03@gmail.com>
2018-10-23 22:26:35 +0530

    form-updated

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import arrow from "./static/arrow.svg";
 import { Form, Text, Select } from "guestbell-forms";
+import DatePicker from "react-datepicker";
 import "./guestbell-forms.css";
+import '../node_modules/react-datepicker/dist/react-datepicker.css';
 
 class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      destination: "",
+      checkIn: "",
+      checkOut: "",
+      roomType: "",
+      adults: 0,
+      children: 0,
+      flexible: 0
+    };
+  }
+
   render() {
     return (
       <div className="outer--main">
@@ -18,14 +34,30 @@ class Details extends Component {
                 <div className="form">
                   <div className="set--one">
                     <div>
-                      <Text required={true} label="Destination" />
+                      <Text
+                        required={true}
+                        label="Destination"
+                        className={
+                          this.state.destination === ""
+                            ? "text-field-error"
+                            : ""
+                        }
+                      />
                     </div>
                     <div className="multiple-fields">
-                      <Text required={true} label="Start Date" />
-                      <Text required={true} label="End Date" />
-                    </div>
-                    <div>
-                      <p>Flexible Travel Dates</p>
+                    <DatePicker>
+                      </DatePicker>
+                      
+                      <img src={arrow} alt="arrow" className="arrow-img" />
+                      <Text
+                        required={true}
+                        label="Check Out"
+                        className={
+                          this.state.checkOut === ""
+                            ? "text-field-error special-cases"
+                            : "special-cases"
+                        }
+                      />
                     </div>
                   </div>
                   <div className="set--two">
@@ -41,11 +73,27 @@ class Details extends Component {
                       />
                     </div>
                     <div className="multiple-fields">
-                      <Text required={true} label="Start Date" />
-                      <Text required={true} label="End Date" />
+                      <Text
+                        required={true}
+                        label="Adults"
+                        className={
+                          this.state.adults === ""
+                            ? "text-field-error special-cases-2"
+                            : "special-cases-2"
+                        }
+                      />
+                      <Text
+                        required={true}
+                        label="Children"
+                        className={
+                          this.state.children === ""
+                            ? "text-field-error special-cases-2"
+                            : "special-cases-2"
+                        }
+                      />
                     </div>
                     <div>
-                      <p>Flexible Travel Dates</p>
+                      <p>Submit</p>
                     </div>
                   </div>
                 </div>
@@ -76,16 +124,34 @@ class Details extends Component {
               margin-top: 20px;
               display: flex;
               flex-direction: row;
-              justify-content: space-between;
+              justify-content: space-around;
             }
-            .set--one, set--two {
+            .set--one, .set--two {
               display: flex;
               flex-direction: column;
-              padding: 0 10px; 
+              width: 45%;
             }
             .multiple-fields {
               display: flex;
               flex-direction: row;
+              align-items: center;
+              justify-content: space-between;
+            }
+            option {
+              background: #a692ff;
+              font-weight: 300;
+            }
+            .arrow-img {
+              width: 30px;
+              height: 9%;
+              margin: 0 0.8em;
+            }
+            .special-cases {
+              margin-right: 0;
+              max-width: 38%;
+            }
+            .special-cases-2 {
+              max-width: 42%;
             }
           `}
         </style>
