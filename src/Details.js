6626df61dff8793a4608fa4f980c825ed55fcafb
Himank Goel <himankgoel03@gmail.com>
2018-10-24 14:33:27 +0530

    Validation Added

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import arrow from "./static/arrow.svg";
+import { Link } from "react-router-dom";
 import { Form, Text, Select } from "guestbell-forms";
-import DatePicker from "react-datepicker";
 import "./guestbell-forms.css";
-import '../node_modules/react-datepicker/dist/react-datepicker.css';
 
 class Details extends Component {
   constructor(props) {
@@ -15,9 +14,72 @@ class Details extends Component {
       checkOut: "",
       roomType: "",
       adults: 0,
-      children: 0,
-      flexible: 0
+      children: 0
     };
+    this.handleDestination = this.handleDestination.bind(this);
+    this.handleCheckIn = this.handleCheckIn.bind(this);
+    this.handleCheckOut = this.handleCheckOut.bind(this);
+    this.handleAdults = this.handleAdults.bind(this);
+    this.handleChildren = this.handleChildren.bind(this);
+    this.handleRoomType = this.handleRoomType.bind(this);
+    this.validate = this.validate.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+  }
+
+  onFocus(e) {
+    e.currentTarget.type = "date";
+  }
+
+  handleDestination(event) {
+    this.setState({
+      destination: event.target.value
+    });
+  }
+
+  handleCheckIn(event) {
+    this.setState({
+      checkIn: event.target.value
+    });
+  }
+
+  handleCheckOut(event) {
+    this.setState({
+      checkOut: event.target.value
+    });
+  }
+
+  handleRoomType(event) {
+    this.setState({
+      roomType: event.target.value
+    });
+  }
+
+  handleAdults(event) {
+    this.setState({
+      adults: parseInt(event.target.value)
+    });
+  }
+
+  handleChildren(event) {
+    this.setState({
+      children: parseInt(event.target.value)
+    });
+  }
+
+  validate() {
+    if (
+      this.state.destination === "" ||
+      this.state.roomType === "" ||
+      this.state.checkIn === "" ||
+      this.state.checkOut === "" ||
+      this.state.children === 0 ||
+      this.state.adults === 0
+    ) {
+      alert("One or more required fields are empty");
+    }
+    else if ((this.state.adults + this.state.children) > 120) {
+      alert("Total booking capacity exceeded");
+    }
   }
 
   render() {
@@ -34,20 +96,32 @@ class Details extends Component {
                 <div className="form">
                   <div className="set--one">
                     <div>
-                      <Text
+                      <Select
+                        className="select-dest"
                         required={true}
-                        label="Destination"
-                        className={
-                          this.state.destination === ""
-                            ? "text-field-error"
-                            : ""
-                        }
+                        label={"Destination"}
+                        values={[
+                          { value: "delhi", label: "New Delhi" },
+                          { value: "mumbai", label: "Mumbai" },
+                          { value: "hydrabad", label: "Hydrabad" },
+                          { value: "kolkata", label: "Kolkata" }
+                        ]}
+                        onChange={this.handleDestination}
                       />
                     </div>
                     <div className="multiple-fields">
-                    <DatePicker>
-                      </DatePicker>
-                      
+                      <Text
+                        required={true}
+                        label="Check In"
+                        className={
+                          this.state.checkIn === ""
+                            ? "text-field-error special-cases"
+                            : "special-cases"
+                        }
+                        onFocus={this.onFocus}
+                        onChange={this.handleCheckIn}
+                        value={this.state.checkIn}
+                      />
                       <img src={arrow} alt="arrow" className="arrow-img" />
                       <Text
                         required={true}
@@ -57,6 +131,9 @@ class Details extends Component {
                             ? "text-field-error special-cases"
                             : "special-cases"
                         }
+                        onFocus={this.onFocus}
+                        onChange={this.handleCheckOut}
+                        value={this.state.checkOut}
                       />
                     </div>
                   </div>
@@ -70,6 +147,7 @@ class Details extends Component {
                           { value: "D", label: "Deluxe" },
                           { value: "SD", label: "Super Deluxe" }
                         ]}
+                        onChange={this.handleRoomType}
                       />
                     </div>
                     <div className="multiple-fields">
@@ -81,6 +159,9 @@ class Details extends Component {
                             ? "text-field-error special-cases-2"
                             : "special-cases-2"
                         }
+                        onChange={this.handleAdults}
+                        value={this.state.adults}
+                        type={'number'}
                       />
                       <Text
                         required={true}
@@ -90,10 +171,20 @@ class Details extends Component {
                             ? "text-field-error special-cases-2"
                             : "special-cases-2"
                         }
+                        onChange={this.handleChildren}
+                        value={this.state.children}
+                        type={'number'}
                       />
                     </div>
-                    <div>
-                      <p>Submit</p>
+                    <div className="btn-postn">
+                      {/* <Link to="/details" replace> */}
+                      <button
+                        type="submit"
+                        className="circle"
+                        value=""
+                        onClick={this.validate}
+                      />
+                      {/* </Link> */}
                     </div>
                   </div>
                 </div>
@@ -141,6 +232,9 @@ class Details extends Component {
               background: #a692ff;
               font-weight: 300;
             }
+            .select-dest option {
+              background: #8fadfe;
+            }
             .arrow-img {
               width: 30px;
               height: 9%;
@@ -152,6 +246,42 @@ class Details extends Component {
             }
             .special-cases-2 {
               max-width: 42%;
+            }
+            .btn-postn {
+              display: flex;
+              flex-direction: row-reverse;
+            }
+            .circle {
+              margin-top: 20px;
+              width: 135px;
+              height: 30px;
+              border-radius: 7px;
+              box-shadow: inset 0 0 0 60px #fff;
+              position: relative;
+              border: none;
+              transition: all 0.5s ease-in-out;
+            }
+            .circle:focus {
+              outline: none;
+            }
+            .circle:hover {
+              background: transparent;
+              box-shadow: inset 0 0 0 2px #fff;
+              cursor: pointer;
+            }
+            .circle:before {
+              content: "Submit";
+              display: block;
+              color: #a494ff;
+              font-size: 15px;
+              position: absolute;
+              top: 43%;
+              left: 38%;
+              margin: -9px 0 0 -5px;
+              transition: all 0.5s ease-in-out;
+            }
+            .circle:hover:before {
+              color: #fff;
             }
           `}
         </style>
